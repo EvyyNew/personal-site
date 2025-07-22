56e3406d8fcd2568dc4a69bbf62b273b586f9e60
Your Name <m15871626653@163.com>
2025-07-21 17:55:19 -0700

    fix: resolve merge conflict in SideBar.js

--- src/components/Template/SideBar.js
+++ src/components/Template/SideBar.js
@@ -22,7 +22,6 @@ const SideBar = () => (
     <section className="blurb">
       <h2>About</h2>
       <p>
-<<<<<<< HEAD
         Hi, I&apos;m Evelyn Yixuan (IPA:[i˥˩ɕyɛn˧˥]) Liu. I&apos;m a recent graduate of{' '}
         <a
           href="https://arts-sciences.buffalo.edu/linguistics/undergraduate/linguistics-degrees/computational-linguistics-ba.html"
@@ -37,13 +36,6 @@ const SideBar = () => (
         </a>{' '}
         founded by Dr. Cassandra Jacobs. My research interests lie at the intersection of large
         language models (LLMs) and phonetic analysis, where I explore the nuances of human language,
-=======
-        Hi, I&apos;m Evelyn Yixuan (IPA:[i˥˩ɕyɛn˧˥]) Liu. I am a recent graduate of{' '}
-        <a href="https://arts-sciences.buffalo.edu/linguistics/undergraduate/linguistics-degrees/computational-linguistics-ba.html">MS in Computational Linguistics at University at Buffalo.</a>{' '}
-        Additionally, I am a member of <a href="https://www.acsu.buffalo.edu/~cxjacobs/index/CaLiCo_Lab.html">CaLiCo Lab</a> founded by Dr. Cassandra Jacobs.
-        My research interests lie at the intersection of large language models (LLMs)
-        and phonetic analysis, where I explore the nuances of human language,
->>>>>>> 6078b5d2cc107192807dd8270085ed5e9d5d8d1c
         speech processing, and AI-driven linguistic models.
       </p>      
     </section>
